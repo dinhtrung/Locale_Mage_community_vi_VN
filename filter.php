5cd02a7ed7fb0aaf85ae41261c84b28c728b462d
Nguyen Dinh Trung <nguyendinhtrung141@gmail.com>
2014-11-21 00:41:49 +0700

    After gtranslate

--- filter.php
+++ filter.php
@@ -1,23 +1,42 @@
 #!/usr/bin/php
 <?php
 
-$infile = $argv[1];
-$outfile = $argv[2];
+$outfile = $infile = $argv[1];
 
 echo "Prepare to process $infile into $outfile \n";
 $fin = fopen($infile, 'r');
-$fout = fopen($outfile, 'w');
+$lines = $translation = [];
 
 while ($line = fgetcsv($fin, 4096)){
-  if ((count($line) == 2) && ($line[0] != $line[1])){
-    fputcsv($fout, $line, ',', '"');
+  if (count($line) == 2) {
+    if ($line[0] != $line[1]){
+	$translation[$line[0]] = $line[1];
+    } elseif (! array_key_exists($line[0], $translation)){
+        $translation[$line[0]] = null;
+    }
   } elseif ((count($line) == 1) && $line[0]) {
-    $gtranslate = shell_exec("trs {en=vi} '$line[0]'");
-    $line[1] = "FIXME: " . $gtranslate;
-    fputcsv($fout, $line, ',', '"');
+    $translation[$line[0]] = null;
+  }
+  if (count($line)){
+     $lines[] = $line;
   }
 }
-fclose($fout);
 fclose($fin);
+foreach ($translation as $source => $trans){
+	if (is_null($trans)){
+		@exec("trans -b {en=vi} '$source'", $trans, $res);
+		if (! $res){
+			$translation[$source] = "@FIXME: " . $trans[0];
+		} else {
+			$translation[$source] = "@TODO";
+		}
+	}
+}
+$fout = fopen($outfile, 'w');
+foreach ($lines as $k => $line){
+	$line[1] = $translation[$line[0]];
+	fputcsv($fout, [$line[0], $line[1]]);
+}
+fclose($fout);
 
 
